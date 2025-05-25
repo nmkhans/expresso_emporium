a43896bf5938a9a89a2ee8693f499916a8fd2e25
nmkhans <nurmoin05@gmail.com>
2025-05-25 22:54:05 +0600

    router setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Outlet } from "react-router";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Outlet />
+    </>
+  );
 };
 
 export default App;
---
+++ src/routes/router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router";
+import App from "../App";
+import Home from "../pages/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+export default router;
